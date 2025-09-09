a328653c8ca1afcadf09fcd90898ec61e306a8c4
Alexander <Alexander.Statsenko@gmail.com>
2025-09-09 10:22:57 +0300

    Create install_and_update.sh

---
+++ install_and_update.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+DESCRIPTION=$(ubus call system board | jsonfilter -e '@.release.description')
+VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
+findKey="OpenWrt"
+findVersion="24.10.2"
+
+install_youtubeunblock_packages() {
+    PKGARCH=$(opkg print-architecture | awk 'BEGIN {max=0} {if ($3 > max) {max = $3; arch = $2}} END {print arch}')
+    VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
+    BASE_URL="https://github.com/Waujito/youtubeUnblock/releases/download/v1.1.0/"
+  	PACK_NAME="youtubeUnblock"
+
+    AWG_DIR="/tmp/$PACK_NAME"
+    mkdir -p "$AWG_DIR"
+    
+    if opkg list-installed | grep -q $PACK_NAME; then
+        echo "$PACK_NAME already installed"
+    else
+	    # Список пакетов, которые нужно проверить и установить/обновить
+		PACKAGES="kmod-nfnetlink-queue kmod-nft-queue kmod-nf-conntrack"
+
+		for pkg in $PACKAGES; do
+			# Проверяем, установлен ли пакет
+			if opkg list-installed | grep -q "^$pkg "; then
+				echo "$pkg already installed"
+			else
+				echo "$pkg not installed. Instal..."
+				opkg install $pkg
+				if [ $? -eq 0 ]; then
+					echo "$pkg file installing successfully"
+				else
+					echo "Error installing $pkg Please, install $pkg manually and run the script again"
+					exit 1
+				fi
+			fi
+		done
+
+        YOUTUBEUNBLOCK_FILENAME="youtubeUnblock-1.1.0-2-2d579d5-${PKGARCH}-openwrt-23.05.ipk"
+        DOWNLOAD_URL="${BASE_URL}${YOUTUBEUNBLOCK_FILENAME}"
+		echo $DOWNLOAD_URL
+        wget -O "$AWG_DIR/$YOUTUBEUNBLOCK_FILENAME" "$DOWNLOAD_URL"
+
+        if [ $? -eq 0 ]; then
+            echo "$PACK_NAME file downloaded successfully"
+        else
+            echo "Error downloading $PACK_NAME. Please, install $PACK_NAME manually and run the script again"
+            exit 1
+        fi
+        
+        opkg install "$AWG_DIR/$YOUTUBEUNBLOCK_FILENAME"
+
+        if [ $? -eq 0 ]; then
+            echo "$PACK_NAME file installing successfully"
+        else
+            echo "Error installing $PACK_NAME. Please, install $PACK_NAME manually and run the script again"
+            exit 1
+        fi
+    fi
+	
+	PACK_NAME="luci-app-youtubeUnblock"
+	if opkg list-installed | grep -q $PACK_NAME; then
+        echo "$PACK_NAME already installed"
+    else
+		PACK_NAME="luci-app-youtubeUnblock"
+		YOUTUBEUNBLOCK_FILENAME="luci-app-youtubeUnblock-1.1.0-1-473af29.ipk"
+        DOWNLOAD_URL="${BASE_URL}${YOUTUBEUNBLOCK_FILENAME}"
+		echo $DOWNLOAD_URL
+        wget -O "$AWG_DIR/$YOUTUBEUNBLOCK_FILENAME" "$DOWNLOAD_URL"
+		
+        if [ $? -eq 0 ]; then
+            echo "$PACK_NAME file downloaded successfully"
+        else
+            echo "Error downloading $PACK_NAME. Please, install $PACK_NAME manually and run the script again"
+            exit 1
+        fi
+        
+        opkg install "$AWG_DIR/$YOUTUBEUNBLOCK_FILENAME"
+
+        if [ $? -eq 0 ]; then
+            echo "$PACK_NAME file installing successfully"
+        else
+            echo "Error installing $PACK_NAME. Please, install $PACK_NAME manually and run the script again"
+            exit 1
+        fi
+	fi
+
+    rm -rf "$AWG_DIR"
+    
+    URL="https://github.com/WebMaster/AX3000T_configs"
+    wget -O "/etc/config/youtubeUnblock" "$URL/config_files/youtubeUnblock"
+    echo youtubeUnblock restart
+    service youtubeUnblock restart
+}
+
+# устанавливаем или обновляем youtubeUnblock и конфиг
+install_youtubeunblock_packages
