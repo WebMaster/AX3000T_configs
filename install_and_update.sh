be6ef3c2572eade6ff3f0dcfab7ade9f22f8b8b9
Alexander <Alexander.Statsenko@gmail.com>
2025-09-24 15:20:58 +0300

    Update install_and_update.sh

--- install_and_update.sh
+++ install_and_update.sh
@@ -10,4 +10,17 @@ lampishe.cc
 '
 uci commit podkop
 service podkop restart
-echo "podkop user domains update successfully"
+echo "Podkop user domains update successfully"
+
+# Добавляем задание на выполнение скрипта каждый день в 4часа 10минут
+#cronTask="*/1 * * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/AX3000T_configs/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
+cronTask="10 4 * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/AX3000T_configs/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
+#str=$(grep -i "\*\/1 \* \* \* \* sh \<\(wget -q -O - https:\/\/raw\.githubusercontent\.com\/WebMaster\/AX3000T_configs\/refs\/heads\/main\/install_and_update\.sh\) 2\>\&1 \| tee \/root\/run\.log" /etc/crontabs/root)
+str=$(grep -i "10 4 \* \* \* sh \<\(wget -q -O - https://raw.githubusercontent.com/WebMaster/AX3000T_configs/refs/heads/main/install_and_update.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
+if [ -z "$str" ] 
+then
+    echo "Add cron task auto reboot service zapret..."
+    echo "$cronTask" >> /etc/crontabs/root
+fi
+
+echo "Complete"
